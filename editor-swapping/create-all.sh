1a235bcb22dc2f456d9a835aa8f73d29134cd786
Sergii Kabashniuk <skabashnyuk@users.noreply.github.com>
2019-05-31 12:39:34 +0300

    Editor swapping demo devfiles (#5)
    Editor swapping demo devfiles (#5)

---
+++ editor-swapping/create-all.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+set -e
+set -u
+
+
+oc login -u developer -p developer >> /dev/null 
+CHE_HOST=$(oc get -o json --namespace  eclipse-che  routes/che -o=go-template='{{ .spec.host }}')
+echo "Using Che api server ${CHE_HOST}"
+find . -name "*.yaml" \
+   -exec echo 'Creating workspace with devfile {}' \; \
+   -exec curl --silent --output /dev/null -X POST --data-binary @{} -H "Content-type: text/x-yaml" http://$CHE_HOST/api/devfile \;
+find . -name "fix.sh" \
+   -exec echo 'Fixing issues {}' \; \
+   -exec '{}'  \;
+
+  
